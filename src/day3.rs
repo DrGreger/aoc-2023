17f1e4d5187a350bc5f7445cde13cc4732af14f8
Simon Herlöfsson <simon.herlofsson@svenskaspel.se>
2023-12-05 22:03:11 +0100

    function to check surrounding chars

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,6 @@
 use crate::read_input;
+use std::cmp::max;
+use std::cmp::min;
 
 pub fn print_solution() {
     static DAY: &str = "day3_ex";
@@ -13,16 +15,28 @@ pub fn print_solution() {
 
 fn part_one(lines: Vec<&str>) -> String {
     let matrix: Vec<Vec<char>> = create_matrix(lines);
+    let mut total_sum = 0;
 
-    for (i, row) in matrix.iter().enumerate() {
-        for (col, symbol) in row.iter().enumerate() {
-            if symbol.is_numeric() {
-                let len = get_number_len(&row, col);
-                println!("Found number of length {} in row {}", len, i)
+    for row in 0..matrix.len() {
+        let mut col = 0;
+        while col < matrix[row].len() {
+            if matrix[row][col].is_numeric() {
+                let len = get_number_len(&matrix[row], col);
+                // println!("Found number of length {} in row {}", len, row);
+                if check_neighbors(&matrix, row, col, len) {
+                    let mut s = String::new();
+                    for i in 0..len {
+                        s.push(matrix[row][col + i])
+                    }
+                    println!("Found number {} in row {}", s.parse::<i32>().unwrap(), row);
+                    total_sum += s.parse::<i32>().unwrap();
+                }
+                col += len;
             }
+            col += 1;
         }
     }
-    println!("\n{}", matrix[0][1]);
+    println!("\n{}", total_sum);
     return "###NOT DONE###".to_string();
 }
 
@@ -38,15 +52,38 @@ fn create_matrix(lines: Vec<&str>) -> Vec<Vec<char>> {
     return matrix;
 }
 
-fn get_number_len(row: &Vec<char>, col: usize) -> i32 {
-    let mut length: i32 = 0;
+fn get_number_len(row: &Vec<char>, col: usize) -> usize {
+    let mut length: usize = 0;
     for index in col..row.len() {
         if row[index].is_numeric() {
-            println!("it's numeric see {}", row[index]);
+            // println!("it's numeric see {}", row[index]);
             length += 1;
         } else {
             break;
         }
     }
     return length;
+}
+
+fn check_neighbors(matrix: &Vec<Vec<char>>, row: usize, col: usize, len: usize) -> bool {
+    let col_min = if col == 0 { 0 } else { col - 1 };
+    let col_max = if col + len == matrix[row].len() {
+        col + len
+    } else {
+        col + len + 1
+    };
+    let row_min = if row == 0 { 0 } else { row - 1 };
+    let row_max = if row == matrix.len() { row } else { row + 2 };
+
+    println!("Checking row {} col {}", row, col);
+    for i in row_min..row_max {
+        for j in col_min..col_max {
+            print!("{}", matrix[i][j]);
+            if matrix[i][j] != '.' && !matrix[i][j].is_numeric() {
+                return true;
+            }
+        }
+        print!("\n")
+    }
+    return false;
 }
